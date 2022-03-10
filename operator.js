7d7d839fd77870d3d0679891bb26ccf35b7786a3
ParthPidurkar <85994476+ParthPidurkar@users.noreply.github.com>
2022-03-11 00:14:33 +0530

    switch case , user input and strict mode.
    npm install readline-sync

---
+++ operator.js
@@ -0,0 +1,22 @@
+'use strict';
+const a = require("readline-sync")
+var input = Number(a.question()) ;
+//input.push(Number(a.question()));
+//var input = 1;
+var x = y+1 ;
+switch(input)
+{
+	case 1:
+	console.log("you have enter number 1");
+	//break;
+	case 2:
+	console.log("you have enter number 2");
+	break;
+	case 3:
+	console.log("you have enter number 3");
+	break;
+	default:
+	console.log("invalid input");
+	break;
+}
+
